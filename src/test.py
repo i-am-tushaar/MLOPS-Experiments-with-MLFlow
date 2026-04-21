916bed51479141f3b8d9201997c6e702133602e3
Tushar <tushaar@example.com>
2026-04-21 17:25:46 +0530

    Done with different experimentation test

---
+++ src/test.py
@@ -0,0 +1,9 @@
+import mlflow
+print("printing tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("\n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+print("Printing new tracking uri")
+print(mlflow.get_tracking_uri())
+print("\n")
